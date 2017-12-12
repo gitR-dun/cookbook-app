665dc76800806082d87bfb183664a713dd3d2a2d
Brian <riceb53@gmail.com>
2017-12-12 17:28:12 -0600

    not sure

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,39 @@
 class Recipe < ApplicationRecord
+  # this is a place where we will be making instance methods
+  # this is where ALL the methods for the recipe class go
+  def as_json
+    {
+      title: title,
+      chef: chef,
+      ingredients: ingredients_list,
+      directions: directions_list,
+      updated_at: day_month_date,
+      prep_time: prep_time_hrs_min
+    }
+  end
+
+  def directions_list
+    directions.split(", ")
+  end
+
+  def ingredients_list
+    ingredients.split(", ")
+  end
+
+  def day_month_date
+    updated_at.strftime("%A, %b %d")
+  end
+
+  def prep_time_hrs_min
+    hours = prep_time / 60
+    minutes = prep_time % 60
+    result = ""
+    if hours > 0
+      result += "#{hours} hours "
+    end
+    if minutes > 0
+      result += "#{minutes} minutes"
+    end
+    result
+  end
 end
